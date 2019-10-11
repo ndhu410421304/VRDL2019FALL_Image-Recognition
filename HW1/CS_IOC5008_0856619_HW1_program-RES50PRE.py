530309be1cb7e14841472a7cf9f164be397fe949
ndhu410421304 <33059129+ndhu410421304@users.noreply.github.com>
2019-10-12 02:27:38 +0800

    remove horizontal, rot to 5

--- HW1/CS_IOC5008_0856619_HW1_program-RES50PRE.py
+++ HW1/CS_IOC5008_0856619_HW1_program-RES50PRE.py
@@ -1,4 +1,4 @@
-#Try more augmentation on images
+#remove horizontal, rot to 5
 import torch
 import torch.nn as nn
 import torch.utils.data as data
@@ -44,8 +44,7 @@ data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomRotation(30),
+        transforms.RandomRotation(5),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
