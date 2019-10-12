70c894cfb8add8417277ab15aa60c02a917ba5c7
ndhu410421304 <33059129+ndhu410421304@users.noreply.github.com>
2019-10-13 01:23:55 +0800

    rot to 10

--- HW1/CS_IOC5008_0856619_HW1_program-RES50PRE.py
+++ HW1/CS_IOC5008_0856619_HW1_program-RES50PRE.py
@@ -44,7 +44,7 @@ data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(5),
+        transforms.RandomRotation(10),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
